b4394d31d9f85401ae4fe616e5d0c6538bc0943d
augustepoiroux <augustepoiroux@gmail.com>
2021-03-02 14:46:21 +0100

    Init commit

--- README.md
+++ README.md
@@ -1,34 +1,36 @@
-# Expressive Power of Invariant and Equivaraint Graph Neural Networks 
+# Expressive Power of Invariant and Equivaraint Graph Neural Networks
 
-In this repository, we show how to use powerful GNN (2-FGNN) to solve a  graph alignment problem. This code was used to derive the practical results in the following paper:
+In this repository, we show how to use powerful GNN (2-FGNN) to solve a graph alignment problem. This code was used to derive the practical results in the following paper:
 
 Waiss Azizian, Marc Lelarge. Expressive Power of Invariant and Equivariant Graph Neural Networks, ICLR 2021.
 
-[arXiv](https://arxiv.org/abs/2006.15646) [OpenReview](https://openreview.net/forum?id=lxHgXYN4bwl) 
+[arXiv](https://arxiv.org/abs/2006.15646) [OpenReview](https://openreview.net/forum?id=lxHgXYN4bwl)
+
 ## Problem: alignment of graphs
+
 The graph isomorphism problem is the computational problem of determining whether two finite graphs are isomorphic. Here we consider a noisy version of this problem: the two graphs below are noisy versions of a parent graph. There is no strict isomorphism between them. Can we still match the vertices of graph 1 with the corresponding vertices of graph 2?
 
-graph 1 | graph 2
-:---:|:---:
-![](images/01_graph1.png) | ![](images/02_graph2.png)
+|          graph 1          |          graph 2          |
+| :-----------------------: | :-----------------------: |
+| ![](images/01_graph1.png) | ![](images/02_graph2.png) |
 
 With our GNN, we obtain the following results: green vertices are well paired vertices and red vertices are errors. Both graphs are now represented using the layout from the right above but the color of the vertices are the same on both sides. At inference, our GNN builds node embedding for the vertices of graphs 1 and 2. Finally a node of graph 1 is matched to its most similar node of graph 2 in this embedding space.
 
-graph 1 | graph 2
-:---:|:---:
-![](images/04_result_graph1.png) | ![](images/03_result_graph2.png)
+|             graph 1              |             graph 2              |
+| :------------------------------: | :------------------------------: |
+| ![](images/04_result_graph1.png) | ![](images/03_result_graph2.png) |
 
 Below, on the left, we plot the errors made by our GNN: errors made on red vertices are represented by links corresponding to a wrong matching or cycle; on the right, we superpose the two graphs: green edges are in both graphs (they correspond to the parent graph), orange edges are in graph 1 only and blue edges are in graph 2 only. We clearly see the impact of the noisy edges (orange and blue) as each red vertex (corresponding to an error) is connected to such edges (except the isolated red vertex).
 
-Wrong matchings/cycles | Superposing the 2 graphs
-:---:|:---:
-![](images/09_preds.png) | ![](images/05_result.png)
+|  Wrong matchings/cycles  | Superposing the 2 graphs  |
+| :----------------------: | :-----------------------: |
+| ![](images/09_preds.png) | ![](images/05_result.png) |
 
-To measure the performance of our GNN, instead of looking at vertices, we can look at edges. On the left below, we see that our GNN recovers most of the green edges present in graphs 1 and 2 (edges from the parent graph). On the right, mismatched edges correspond mostly to noisy (orange and blue) edges (present in only one of the graphs 1 or 2). 
+To measure the performance of our GNN, instead of looking at vertices, we can look at edges. On the left below, we see that our GNN recovers most of the green edges present in graphs 1 and 2 (edges from the parent graph). On the right, mismatched edges correspond mostly to noisy (orange and blue) edges (present in only one of the graphs 1 or 2).
 
-Matched edges | Mismatched edges
-:---:|:---:
-![](images/07_match.png) | ![](images/08_mismatch.png)
+|      Matched edges       |      Mismatched edges       |
+| :----------------------: | :-------------------------: |
+| ![](images/07_match.png) | ![](images/08_mismatch.png) |
 
 ## Training GNN for the graph alignment problem
 
@@ -51,6 +53,7 @@ We provide a simple [notebook](https://github.com/mlelarge/graph_neural_net/blob
 We refer to our [paper](https://openreview.net/forum?id=lxHgXYN4bwl) for comparisons with other algorithms (message passing GNN, spectral or SDP algorithms).
 
 To cite our paper:
+
 ```
 @inproceedings{azizian2020characterizing,
   title={Expressive power of invariant and equivariant graph neural networks},
@@ -62,6 +65,7 @@ To cite our paper:
 ```
 
 ## Overview of the code
+
 ### Project structure
 
 ```bash
@@ -70,7 +74,7 @@ To cite our paper:
 |   └── dataset selector
 |   └── data_generator.py # generating random graphs
 |   └── test_data_generator.py
-|   └── siamese_loader.py # loading pairs 
+|   └── siamese_loader.py # loading pairs
 ├── models
 |   └── architecture selector
 |   └── layers.py # equivariant block
@@ -89,26 +93,35 @@ To cite our paper:
 ├── eval.py # testing models
 ```
 
-
 ## Dependencies
+
 Dependencies are listed in `requirements.txt`. To install, run
+
 ```
 pip install -r requirements.txt
 ```
-## Training 
-Run the main file ```commander.py``` with the command ```train```
+
+## Training
+
+Run the main file `commander.py` with the command `train`
+
 ```
-python train commander.py
+python commander.py train
 ```
-To change options, use [Sacred](https://github.com/IDSIA/sacred) command-line interface and see ```default.yaml``` for the configuration structure. For instance,
+
+To change options, use [Sacred](https://github.com/IDSIA/sacred) command-line interface and see `default.yaml` for the configuration structure. For instance,
+
 ```
-python commander.py train with cpu=No data.generative_model=Regular train.epoch=10 
+python commander.py train with cpu=No data.generative_model=Regular train.epoch=10
 ```
-You can also copy ```default.yaml``` and modify the configuration parameters there. Loading the configuration in ```other.yaml``` (or ```other.json```) can be done with
+
+You can also copy `default.yaml` and modify the configuration parameters there. Loading the configuration in `other.yaml` (or `other.json`) can be done with
+
 ```
 python commander.py train with other.yaml
 ```
-See [Sacred documentation](http://sacred.readthedocs.org/) for an exhaustive reference. 
+
+See [Sacred documentation](http://sacred.readthedocs.org/) for an exhaustive reference.
 
 To save logs to [Neptune](https://neptune.ai/), you need to provide your own API key via the dedicated environment variable.
 
@@ -116,13 +129,16 @@ The model is regularly saved in the folder `runs`.
 
 ## Evaluating
 
-There are two ways of evaluating the models. If you juste ran the training with a configuration ```conf.yaml```, you can simply do,
+There are two ways of evaluating the models. If you juste ran the training with a configuration `conf.yaml`, you can simply do,
+
 ```
 python commander.py eval with conf.yaml
 ```
-You can omit ```with conf.yaml``` if you are using the default configuartion.
 
-If you downloaded a model with a config file from here, you can edit the section ```test_data``` of this config if you wish and then run,
+You can omit `with conf.yaml` if you are using the default configuartion.
+
+If you downloaded a model with a config file from here, you can edit the section `test_data` of this config if you wish and then run,
+
 ```
 python commander.py eval with /path/to/config model_path=/path/to/model.pth.tar
 ```
--- commander.py
+++ commander.py
@@ -16,50 +16,62 @@ from toolbox import utils
 import trainer as trainer
 
 from sacred import SETTINGS
+
 SETTINGS.CONFIG.READ_ONLY_CONFIG = False
 
 ### BEGIN Sacred setup
 ex = Experiment()
-ex.add_config('default_qap.yaml')
+ex.add_config("default_qap.yaml")
+
 
 @ex.config_hook
 def set_experiment_name(config, command_name, logger):
-    ex.path = config['name']
+    ex.path = config["name"]
     return config
+
 
 @ex.config
 def update_paths(root_dir, name, train_data, test_data):
-    log_dir = '{}/runs/{}/QAP_{}_{}_{}_{}_{}_{}/'.format(
-              root_dir, name, train_data['generative_model'],
-              train_data['noise_model'], train_data['n_vertices'],
-              train_data['vertex_proba'], train_data['noise'],
-              train_data['edge_density'])
-    path_dataset = train_data['path_dataset']
+    log_dir = "{}/runs/{}/QAP_{}_{}_{}_{}_{}_{}/".format(
+        root_dir,
+        name,
+        train_data["generative_model"],
+        train_data["noise_model"],
+        train_data["n_vertices"],
+        train_data["vertex_proba"],
+        train_data["noise"],
+        train_data["edge_density"],
+    )
+    path_dataset = train_data["path_dataset"]
     # The two keys below are specific to testing
     # These default values are overriden by command line
-    model_path = os.path.join(log_dir, 'model_best.pth.tar')
-    output_filename = 'test.json'
+    model_path = os.path.join(log_dir, "model_best.pth.tar")
+    output_filename = "test.json"
+
 
 @ex.config_hook
 def init_observers(config, command_name, logger):
-    if command_name == 'train':
-        neptune = config['observers']['neptune']
-        if neptune['enable']:
+    if command_name == "train":
+        neptune = config["observers"]["neptune"]
+        if neptune["enable"]:
             from neptunecontrib.monitoring.sacred import NeptuneObserver
-            ex.observers.append(NeptuneObserver(project_name=neptune['project']))
+
+            ex.observers.append(NeptuneObserver(project_name=neptune["project"]))
     return config
+
 
 @ex.post_run_hook
 def clean_observer(observers):
     """ Observers that are added in a config_hook need to be cleaned """
     try:
-        neptune = observers['neptune']
-        if neptune['enable']:
+        neptune = observers["neptune"]
+        if neptune["enable"]:
             from neptunecontrib.monitoring.sacred import NeptuneObserver
-            ex.observers = [obs for obs in ex.observers
-                            if not isinstance(obs, NeptuneObserver)]
+
+            ex.observers = [obs for obs in ex.observers if not isinstance(obs, NeptuneObserver)]
     except KeyError:
         pass
+
 
 ### END Sacred setup
 
@@ -68,12 +80,13 @@ def clean_observer(observers):
 def init_logger(name, _config, _run):
     # set loggers
     exp_logger = logger.Experiment(name, _config, run=_run)
-    exp_logger.add_meters('train', metrics.make_meter_matching())
-    exp_logger.add_meters('val', metrics.make_meter_matching())
-    #exp_logger.add_meters('test', metrics.make_meter_matching())
-    exp_logger.add_meters('hyperparams', {'learning_rate': metrics.ValueMeter()})
+    exp_logger.add_meters("train", metrics.make_meter_matching())
+    exp_logger.add_meters("val", metrics.make_meter_matching())
+    # exp_logger.add_meters('test', metrics.make_meter_matching())
+    exp_logger.add_meters("hyperparams", {"learning_rate": metrics.ValueMeter()})
     return exp_logger
- 
+
+
 @ex.capture
 def setup_env(cpu):
     # Randomness is already controlled by Sacred
@@ -81,32 +94,34 @@ def setup_env(cpu):
     if not cpu:
         cudnn.benchmark = True
 
+
 # create necessary folders and config files
 @ex.capture
 def init_output_env(_config, root_dir, log_dir, path_dataset):
-    utils.check_dir(os.path.join(root_dir,'runs'))
+    utils.check_dir(os.path.join(root_dir, "runs"))
     utils.check_dir(log_dir)
     utils.check_dir(path_dataset)
-    with open(os.path.join(log_dir, 'config.json'), 'w') as f:
+    with open(os.path.join(log_dir, "config.json"), "w") as f:
         json.dump(_config, f)
 
+
 @ex.capture
-def save_checkpoint(state, is_best, log_dir, filename='checkpoint.pth.tar'):
+def save_checkpoint(state, is_best, log_dir, filename="checkpoint.pth.tar"):
     utils.check_dir(log_dir)
     filename = os.path.join(log_dir, filename)
     torch.save(state, filename)
     if is_best:
-        shutil.copyfile(filename, os.path.join(log_dir, 'model_best.pth.tar'))
+        shutil.copyfile(filename, os.path.join(log_dir, "model_best.pth.tar"))
 
-    fn = os.path.join(log_dir, 'checkpoint_epoch{}.pth.tar')
-    torch.save(state, fn.format(state['epoch']))
+    fn = os.path.join(log_dir, "checkpoint_epoch{}.pth.tar")
+    torch.save(state, fn.format(state["epoch"]))
 
-    if (state['epoch'] - 1 ) % 5 != 0:
-      #remove intermediate saved models, e.g. non-modulo 5 ones
-      if os.path.exists(fn.format(state['epoch'] - 1 )):
-          os.remove(fn.format(state['epoch'] - 1 ))
+    if (state["epoch"] - 1) % 5 != 0:
+        # remove intermediate saved models, e.g. non-modulo 5 ones
+        if os.path.exists(fn.format(state["epoch"] - 1)):
+            os.remove(fn.format(state["epoch"] - 1))
 
-    state['exp_logger'].to_json(log_dir=log_dir,filename='logger.json')
+    state["exp_logger"].to_json(log_dir=log_dir, filename="logger.json")
 
 
 @ex.command
@@ -116,53 +131,76 @@ def train(cpu, train_data, train, arch, log_dir):
     global best_score, best_epoch
     best_score, best_epoch = -1, -1
     use_cuda = not cpu and torch.cuda.is_available()
-    device = 'cuda' if use_cuda else 'cpu'
-    print('Using device:', device)
+    device = "cuda" if use_cuda else "cpu"
+    print("Using device:", device)
 
-    # init random seeds 
+    # init random seeds
     setup_env()
     print("Models saved in ", log_dir)
 
     init_output_env()
     exp_logger = init_logger()
-    
-    gene_train = Generator('train', train_data)
+
+    gene_train = Generator("train", train_data)
     gene_train.load_dataset()
-    train_loader = siamese_loader(gene_train, train['batch_size'],
-                                  gene_train.constant_n_vertices)
-    gene_val = Generator('val', train_data)
+    train_loader = siamese_loader(gene_train, train["batch_size"], gene_train.constant_n_vertices)
+    gene_val = Generator("val", train_data)
     gene_val.load_dataset()
-    val_loader = siamese_loader(gene_val, train['batch_size'],
-                                gene_val.constant_n_vertices)
-    
+    val_loader = siamese_loader(gene_val, train["batch_size"], gene_val.constant_n_vertices)
+
     model = get_model(arch)
-    optimizer, scheduler = get_optimizer(train,model)
-    criterion = get_criterion(device, train['loss_reduction'])
+
+    print(0, model.node_embedder.base_model.reg_blocks)
+    print(1, model.node_embedder.base_model.reg_blocks[0])
+    model.node_embedder.base_model.reg_blocks[0].requires_grad = False
+
+    optimizer, scheduler = get_optimizer(train, model)
+    criterion = get_criterion(device, train["loss_reduction"])
 
     model.to(device)
 
     is_best = True
-    for epoch in range(train['epoch']):
-        print('Current epoch: ', epoch)
-        trainer.train_triplet(train_loader,model,criterion,optimizer,exp_logger,device,epoch,eval_score=metrics.accuracy_max,print_freq=train['print_freq'])
-        
+    for epoch in range(train["epoch"]):
+        print("Current epoch: ", epoch)
+        trainer.train_triplet(
+            train_loader,
+            model,
+            criterion,
+            optimizer,
+            exp_logger,
+            device,
+            epoch,
+            eval_score=metrics.accuracy_max,
+            print_freq=train["print_freq"],
+        )
 
-
-        acc, loss = trainer.val_triplet(val_loader,model,criterion,exp_logger,device,epoch,eval_score=metrics.accuracy_linear_assignment)
+        acc, loss = trainer.val_triplet(
+            val_loader,
+            model,
+            criterion,
+            exp_logger,
+            device,
+            epoch,
+            eval_score=metrics.accuracy_linear_assignment,
+        )
         scheduler.step(loss)
         # remember best acc and save checkpoint
-        is_best = (acc > best_score)
+        is_best = acc > best_score
         best_score = max(acc, best_score)
         if True == is_best:
             best_epoch = epoch
-            
-        save_checkpoint({
-            'epoch': epoch + 1,
-            'state_dict': model.state_dict(),
-            'best_score': best_score,
-            'best_epoch': best_epoch,
-            'exp_logger': exp_logger,
-            }, is_best)
+
+        save_checkpoint(
+            {
+                "epoch": epoch + 1,
+                "state_dict": model.state_dict(),
+                "best_score": best_score,
+                "best_epoch": best_epoch,
+                "exp_logger": exp_logger,
+            },
+            is_best,
+        )
+
 
 ### Testing
 @ex.capture
@@ -171,10 +209,11 @@ def load_model(model, device, model_path):
     if os.path.exists(model_path):
         print("Reading model from ", model_path)
         checkpoint = torch.load(model_path, map_location=torch.device(device))
-        model.load_state_dict(checkpoint['state_dict'])
+        model.load_state_dict(checkpoint["state_dict"])
         return model
     else:
-        raise RuntimeError('Model does not exist!')
+        raise RuntimeError("Model does not exist!")
+
 
 def save_to_json(key, acc, loss, filename):
     if os.path.exists(filename):
@@ -182,44 +221,59 @@ def save_to_json(key, acc, loss, filename):
             data = json.load(jsonFile)
     else:
         data = {}
-    data[key] = {'loss':loss, 'acc': acc}
-    with open(filename, 'w') as jsonFile:
+    data[key] = {"loss": loss, "acc": acc}
+    with open(filename, "w") as jsonFile:
         json.dump(data, jsonFile)
+
 
 @ex.capture
 def create_key(log_dir, test_data):
-    template = 'model_{}data_QAP_{}_{}_{}_{}_{}_{}_{}'
-    key=template.format(log_dir, test_data['generative_model'], test_data['noise_model'],
-                        test_data['num_examples_test'], test_data['n_vertices'],
-                        test_data['vertex_proba'], test_data['noise'],
-                        test_data['edge_density'])
+    template = "model_{}data_QAP_{}_{}_{}_{}_{}_{}_{}"
+    key = template.format(
+        log_dir,
+        test_data["generative_model"],
+        test_data["noise_model"],
+        test_data["num_examples_test"],
+        test_data["n_vertices"],
+        test_data["vertex_proba"],
+        test_data["noise"],
+        test_data["edge_density"],
+    )
     return key
+
 
 @ex.command
 def eval(name, cpu, test_data, train, arch, log_dir, model_path, output_filename):
     use_cuda = not cpu and torch.cuda.is_available()
-    device = 'cuda' if use_cuda else 'cpu'
-    print('Using device:', device)
+    device = "cuda" if use_cuda else "cpu"
+    print("Using device:", device)
 
     model = get_model(arch)
     model.to(device)
     model = load_model(model, device)
 
-    criterion = get_criterion(device, train['loss_reduction'])
+    criterion = get_criterion(device, train["loss_reduction"])
     exp_logger = logger.Experiment(name)
-    exp_logger.add_meters('test', metrics.make_meter_matching())
+    exp_logger.add_meters("test", metrics.make_meter_matching())
 
-    gene_test = Generator('test', test_data)
+    gene_test = Generator("test", test_data)
     gene_test.load_dataset()
-    test_loader = siamese_loader(gene_test, train['batch_size'],
-                                 gene_test.constant_n_vertices)
-    acc, loss = trainer.val_triplet(test_loader, model, criterion, exp_logger, device,
-                                    epoch=0, eval_score=metrics.accuracy_linear_assignment,
-                                    val_test='test')
+    test_loader = siamese_loader(gene_test, train["batch_size"], gene_test.constant_n_vertices)
+    acc, loss = trainer.val_triplet(
+        test_loader,
+        model,
+        criterion,
+        exp_logger,
+        device,
+        epoch=0,
+        eval_score=metrics.accuracy_linear_assignment,
+        val_test="test",
+    )
     key = create_key()
-    filename_test = os.path.join(log_dir,  output_filename)
-    print('Saving result at: ',filename_test)
+    filename_test = os.path.join(log_dir, output_filename)
+    print("Saving result at: ", filename_test)
     save_to_json(key, acc, loss, filename_test)
+
 
 @ex.automain
 def main():
