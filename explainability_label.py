927e1a712a8daac8a62302b6724fba288db5ed96
Lucas Guirardel <lucas.guirardel@polytechnique.edu>
2021-03-08 12:38:17 +0100

    Similarity viz

--- explainability_label.py
+++ explainability_label.py
@@ -13,44 +13,36 @@ import torch
 from sklearn.manifold import TSNE
 import numpy as np
 
-data_path = "dataset/QAP_steps_ErdosRenyi_100_25_1.0_0.1_0.2/test.pkl"
-model_path = "runs/Reg-ER-100/QAP_ErdosRenyi_ErdosRenyi_25_1.0_0.05_0.2"
+data_path = "dataset/labels_1000_ErdosRenyi_0.5_ErdosRenyi_0.5_0.2/test.pkl"
+model_path = "./runs/Reg-ER-100-nodes-classification/labels_ErdosRenyi_0.5_ErdosRenyi_0.5_0.2"
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-def get_embeddings(model, g1, g2=None):
-    """Take g1, g2 1-batches of graph
+def get_embeddings(model, g1):
+    """Take g1 1-batches of graph
     returns embeddings of shape (1,n, embed_dim)"""
     embeddings = []
     handle = model.node_embedder.register_forward_hook(lambda module, inp, outp : embeddings.append(outp))
-    if g2 is None :
-        model(g1)
-    else:
-        model(g1,g2)
+    sim = model(g1)
+
     handle.remove()
-    return embeddings
+    return embeddings[0], sim
 
 def get_graphs(data, i, device):
     print(f"Using data point nb {args.i}")
-    g1, g2 = data[args.i] #graphs !
+    g1, _ = data[args.i] #graphs !
     g1.unsqueeze_(0) #batches of 1
-    g2.unsqueeze_(0)
     print(g1.shape)
     g1 = g1.to(device)
-    g2 = g2.to(device)
 
-    return g1,g2
+    return g1
 
 
-def embed(g1,g2, model):
-    e1, e2 = get_embeddings(model, g1, g2)
-    e = torch.cat((e1,e2), axis = 1)
+def embed(g1, model):
+    e, sim = get_embeddings(model, g1)
     tsne = TSNE()
     v = tsne.fit_transform(e.cpu().detach().squeeze().numpy())
-    _,n,_ = e1.shape
-    v1 = v[:n, :]
-    v2 = v[n:, :]
-    return v1, v2
+    return v, sim
 
 if __name__ == "__main__":
     parser = ArgumentParser()
@@ -72,14 +64,13 @@ if __name__ == "__main__":
     model.eval()
     model.to(device)
     model = load_model(model, device, os.path.join(model_path,"model_best.pth.tar"))
-    g1,g2 = get_graphs(data,args.i, device)
+    g1= get_graphs(data,args.i, device)
     
-    e1, e2 = embed(g1, g2, model)
+    e1, sim = embed(g1, model)
     #embeddings
     np.save("embeds/g1/embeds", e1)
-    np.save("embeds/g2/embeds", e2)
     #original graphs
     np.save("embeds/g1/graph", g1.detach().cpu().numpy())
-    np.save("embeds/g2/graph", g2.detach().cpu().numpy())
     #similarity matrices
+    np.save("embeds/g1/simil", sim.detach().cpu().numpy())
 
